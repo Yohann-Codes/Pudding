6a20f0c207039823a1e9786dfb3f83ce503738aa
Yohann-Codes <yohann_mail@yeah.net>
2017-03-26 11:40:55 +0800

    心跳包可忽略高地址的4位序列化/反序列化标志

--- pudding-common/src/main/java/org/pudding/common/utils/ProtocolHeaderFactory.java
+++ pudding-common/src/main/java/org/pudding/common/utils/ProtocolHeaderFactory.java
@@ -16,7 +16,8 @@ public class ProtocolHeaderFactory {
     public static ProtocolHeader newHeartbeatHeader() {
         ProtocolHeader header = new ProtocolHeader();
         header.setMagic(ProtocolHeader.MAGIC);
-        header.setType(ProtocolHeader.type(PuddingConfig.serializerType, ProtocolHeader.HEATBEAT));
+        // 心跳包可忽略高地址的4位序列化/反序列化标志
+        header.setType(ProtocolHeader.type((byte) 0, ProtocolHeader.HEATBEAT));
         header.setSign((byte) 0);
         header.setId(0);
         header.setErrorCode(0);
