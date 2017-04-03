29459c41e94d2249e1a1c23a8167019e82d464e9
Yohann-Codes <yohann_mail@yeah.net>
2017-04-03 22:36:43 +0800

    异步调用

---
+++ pudding-rpc/src/main/java/org/pudding/rpc/utils/InvokeMap.java
@@ -0,0 +1,47 @@
+package org.pudding.rpc.utils;
+
+import org.pudding.common.model.Result;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * ResultMap.
+ *   key --> invokeId
+ * value --> Result
+ *
+ * @author Yohann.
+ */
+public class ResultMap {
+    private Map<String, Result> results;
+
+    public ResultMap() {
+        results = new HashMap<>();
+    }
+
+    public void put(long invokeId, Result result) {
+        results.put(key(invokeId), result);
+    }
+
+    public Result get(long invokeId) {
+        return results.get(key(invokeId));
+    }
+
+    public boolean containsKey(long invokeId) {
+        return results.containsKey(key(invokeId));
+    }
+
+    public void remove(long invokeId) {
+        results.remove(key(invokeId));
+    }
+
+    public int size() {
+        return results.size();
+    }
+
+    private String key(long invokeId) {
+        @SuppressWarnings("unchecked")
+        String key = String.valueOf(invokeId);
+        return key;
+    }
+}
