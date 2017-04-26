5c7f3c707c2ba721876e108f5db0c24867c0a5be
Yohann-Codes <yohann_mail@yeah.net>
2017-04-26 18:49:22 +0800

    ignore

--- pudding-example/src/main/java/org/pudding/example/consumer/ConsumerServer_Test.java
+++ pudding-example/src/main/java/org/pudding/example/consumer/ConsumerServer_Test.java
@@ -6,43 +6,107 @@ import org.pudding.common.exception.ServiceBusyException;
 import org.pudding.common.model.ServiceMeta;
 import org.pudding.example.service.ServiceA;
 import org.pudding.example.service.ServiceB;
+import org.pudding.example.service.ServiceC;
+import org.pudding.example.service.ServiceD;
 import org.pudding.rpc.DefaultMetaFactory;
 import org.pudding.rpc.consumer.DefaultServiceConsumer;
 import org.pudding.rpc.consumer.ServiceConsumer;
 import org.pudding.rpc.consumer.invoker.ServiceProxyFactory;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 /**
  * @author Yohann.
  */
-public class ConsumerServer_1 {
+public class ConsumerServer_Test {
     public static void main(String[] args) {
-        // 创建消费者并连接注册中心
         ServiceConsumer consumer = new DefaultServiceConsumer();
         consumer.connectRegistry("127.0.0.1:20001", "127.0.0.1:20002", "127.0.0.1:20003", "127.0.0.1:20004");
 
-        // 创建需要订阅服务的元数据
         ServiceMeta serviceMetaA = DefaultMetaFactory.createSubscribeMeta(ServiceA.class.getName());
         ServiceMeta serviceMetaB = DefaultMetaFactory.createSubscribeMeta(ServiceB.class.getName());
+        ServiceMeta serviceMetaC = DefaultMetaFactory.createSubscribeMeta(ServiceC.class.getName());
+        ServiceMeta serviceMetaD = DefaultMetaFactory.createSubscribeMeta(ServiceD.class.getName());
 
-        // 订阅服务(同步方式，订阅成功后返回)
-        consumer.subscribeServices(serviceMetaA, serviceMetaB);
+        consumer.subscribeServices(serviceMetaA, serviceMetaB, serviceMetaC, serviceMetaD);
 
-        // 创建同步调用代理服务
-        ServiceA serviceA = ServiceProxyFactory.createSyncProxy(ServiceA.class);
-        ServiceB serviceB = ServiceProxyFactory.createSyncProxy(ServiceB.class);
+        final ServiceA serviceA = ServiceProxyFactory.createSyncProxy(ServiceA.class);
+        final ServiceB serviceB = ServiceProxyFactory.createSyncProxy(ServiceB.class);
+        final ServiceC serviceC = ServiceProxyFactory.createSyncProxy(ServiceC.class);
+        final ServiceD serviceD = ServiceProxyFactory.createSyncProxy(ServiceD.class);
 
-        // 同步调用(监听器参数传null即可)
-        try {
-            int add = serviceA.add(100, 200, null);
-            System.out.println("100 + 200 = " + add);
-            int subtract = serviceB.subtract(200, 100, null);
-            System.out.println("200 - 100 = " + subtract);
-        } catch (InvokeTimeoutException e) {
-            System.out.println("调用超时");
-        } catch (NotFindServiceException e) {
-            System.out.println("未找到服务");
-        } catch (ServiceBusyException e) {
-            System.out.println("服务繁忙");
+        ExecutorService executor = Executors.newFixedThreadPool(5);
+        for (;;) {
+            executor.execute(new Runnable() {
+                @Override
+                public void run() {
+                    try {
+                        int add = serviceA.add(100, 200, null);
+                        System.out.println("100 + 200 = " + add);
+                    } catch (InvokeTimeoutException e) {
+                        System.out.println("调用超时");
+                    } catch (NotFindServiceException e) {
+                        System.out.println("未找到服务");
+                    } catch (ServiceBusyException e) {
+                        System.out.println("服务繁忙");
+                    }
+                }
+            });
+
+            executor.execute(new Runnable() {
+                @Override
+                public void run() {
+                    try {
+                        int subtract = serviceB.subtract(200, 100, null);
+                        System.out.println("200 - 100 = " + subtract);
+                    } catch (InvokeTimeoutException e) {
+                        System.out.println("调用超时");
+                    } catch (NotFindServiceException e) {
+                        System.out.println("未找到服务");
+                    } catch (ServiceBusyException e) {
+                        System.out.println("服务繁忙");
+                    }
+                }
+            });
+
+            executor.execute(new Runnable() {
+                @Override
+                public void run() {
+                    try {
+                        int multiply = serviceC.multiply(100, 200, null);
+                        System.out.println("200 * 100 = " + multiply);
+                    } catch (InvokeTimeoutException e) {
+                        System.out.println("调用超时");
+                    } catch (NotFindServiceException e) {
+                        System.out.println("未找到服务");
+                    } catch (ServiceBusyException e) {
+                        System.out.println("服务繁忙");
+                    }
+                }
+            });
+
+            executor.execute(new Runnable() {
+                @Override
+                public void run() {
+                    try {
+                        int divide = serviceD.divide(200, 100, null);
+                        System.out.println("200 / 100 = " + divide);
+                    } catch (InvokeTimeoutException e) {
+                        System.out.println("调用超时");
+                    } catch (NotFindServiceException e) {
+                        System.out.println("未找到服务");
+                    } catch (ServiceBusyException e) {
+                        System.out.println("服务繁忙");
+                    }
+                }
+            });
+
+            try {
+                Thread.sleep(300);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
         }
     }
 }
